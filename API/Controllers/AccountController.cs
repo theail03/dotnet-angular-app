8d9bb3311cfda1389523864c6df8f0d37c296452
theail03 <andresirias03@gmail.com>
2024-01-31 23:01:53 -0600

    Add loginWithGoogle method in API

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using API.DTOs;
 using API.Entities;
 using API.Interfaces;
 using AutoMapper;
+using Google.Apis.Auth;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -16,11 +17,13 @@ namespace API.Controllers
         private readonly UserManager<AppUser> _userManager;
         private readonly ITokenService _tokenService;
         private readonly IMapper _mapper;
-        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, IMapper mapper)
+        private readonly string _googleClientId;
+        public AccountController(IConfiguration config, UserManager<AppUser> userManager, ITokenService tokenService, IMapper mapper)
         {
             _userManager = userManager;
             _mapper = mapper;
             _tokenService = tokenService;
+            _googleClientId = config["ApplicationSettings:GoogleClientId"];
         }
 
         [HttpPost("register")] // POST: api/account/register?username=dave&password=pwd
@@ -48,6 +51,61 @@ namespace API.Controllers
                 Gender = user.Gender
             };
         }
+
+    [HttpPost("loginWithGoogle")]
+    public async Task<ActionResult<UserDto>> LoginWithGoogle(string credential)
+    {
+        var settings = new GoogleJsonWebSignature.ValidationSettings()
+        {
+            Audience = new List<string> { _googleClientId }
+        };
+
+        GoogleJsonWebSignature.Payload payload;
+        try
+        {
+            payload = await GoogleJsonWebSignature.ValidateAsync(credential, settings);
+        }
+        catch (Exception)
+        {
+            return BadRequest("Invalid Google token.");
+        }
+
+        // Check if the user exists
+        var user = await _userManager.Users.FirstOrDefaultAsync(u => u.GoogleId == payload.Subject);
+        if (user == null)
+        {
+            // User does not exist, create a new one
+            user = new AppUser
+            {
+                UserName = payload.Email,
+                Email = payload.Email,
+                GoogleId = payload.Subject
+            };
+
+            var createResult = await _userManager.CreateAsync(user);
+            if (!createResult.Succeeded)
+            {
+                return BadRequest(createResult.Errors);
+            }
+
+            // Assign role to new users
+            var roleResult = await _userManager.AddToRoleAsync(user, "Member");
+            if (!roleResult.Succeeded)
+            {
+                return BadRequest(roleResult.Errors);
+            }
+        }
+
+        // Generate token
+        return new UserDto
+        {
+            Username = user.UserName,
+            Token = await _tokenService.CreateToken(user),
+            PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
+            KnownAs = user.KnownAs,
+            Gender = user.Gender
+        };
+    }
 
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
