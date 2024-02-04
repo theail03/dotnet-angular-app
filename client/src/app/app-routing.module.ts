8314ffd97e91953acc40be5c57bd4c6a731c41da
theail03 <andresirias03@gmail.com>
2024-02-03 18:15:46 -0600

    Update dictionary component in routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ import { AdminGuard } from './_guards/admin.guard';
 import { AuthGuard } from './_guards/auth.guard';
 import { PreventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 import { MemberDetailedResolver } from './_resolvers/member-detailed.resolver';
+import { DictionaryComponent } from './dictionary/dictionary.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -27,7 +28,7 @@ const routes: Routes = [
       {path: 'lists', component: ListsComponent},
       {path: 'messages', component: MessagesComponent},
       {path: 'admin', component: AdminPanelComponent, canActivate: [AdminGuard]},
-      {path: 'dictionary/create', component: MemberEditComponent, canDeactivate: [PreventUnsavedChangesGuard]},
+      {path: 'dictionary/create', component: DictionaryComponent, canDeactivate: [PreventUnsavedChangesGuard]},
     ]
   },
   {path: 'errors', component: TestErrorComponent},
