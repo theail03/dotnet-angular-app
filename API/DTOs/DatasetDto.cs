cd5ba92681926b7bb9c940127c912ab005888909
theail03 <andresirias03@gmail.com>
2024-02-11 15:26:23 -0600

    Add more fields to dataset

--- API/DTOs/DatasetDto.cs
+++ API/DTOs/DatasetDto.cs
@@ -6,5 +6,7 @@
         public string Title { get; set; }
         public string Description { get; set; }
         public string CsvContent { get; set; }
+        public bool IsPublic { get; set; }
+        public int AppUserId { get; set; }
     }
 }
---
+++ API/Data/Migrations/20240211211906_AddAdditionalDatasetFields.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdditionalDatasetFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AppUserId",
+                table: "Datasets",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPublic",
+                table: "Datasets",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Datasets_AppUserId",
+                table: "Datasets",
+                column: "AppUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Datasets_AspNetUsers_AppUserId",
+                table: "Datasets",
+                column: "AppUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Datasets_AspNetUsers_AppUserId",
+                table: "Datasets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Datasets_AppUserId",
+                table: "Datasets");
+
+            migrationBuilder.DropColumn(
+                name: "AppUserId",
+                table: "Datasets");
+
+            migrationBuilder.DropColumn(
+                name: "IsPublic",
+                table: "Datasets");
+        }
+    }
+}
