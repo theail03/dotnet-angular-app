313b6381ad8c8dbe91a1c6bcac9fabc50b35c9bc
theail03 <andresirias03@gmail.com>
2024-02-12 13:52:30 -0600

    Restore register validation rules for testing validation errors

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -5,14 +5,16 @@ namespace API.DTOs
 {
     public class RegisterDto
     {
+        [Required]
         public string Username { get; set; }
 
-        public string KnownAs { get; set; }
-        public string Gender { get; set; }
-        public DateOnly? DateOfBirth { get; set; } // optional to make required work!
-        public string City { get; set; }
-        public string Country { get; set; }
+        [Required] public string KnownAs { get; set; }
+        [Required] public string Gender { get; set; }
+        [Required] public DateOnly? DateOfBirth { get; set; } // optional to make required work!
+        [Required] public string City { get; set; }
+        [Required] public string Country { get; set; }
 
+        [Required]
         [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
         public string GoogleId { get; set; }
