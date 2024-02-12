bd87b8d0bd167084d628c7c31e1af2914fe96b04
theail03 <andresirias03@gmail.com>
2024-02-11 23:15:51 -0600

    Add required to fields in DatasetDto to send BadRequest exceptions

--- API/DTOs/DatasetDto.cs
+++ API/DTOs/DatasetDto.cs
@@ -1,12 +1,14 @@
-﻿namespace API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
 {
     public class DatasetDto
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
         public string Description { get; set; }
+        [Required]
         public string CsvContent { get; set; }
-        public bool IsPublic { get; set; }
-        public int AppUserId { get; set; }
     }
 }
