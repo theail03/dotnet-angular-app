f5a8199f7ad4b552e3cfd05892e0d4751701036c
theail03 <andresirias03@gmail.com>
2024-02-22 21:25:27 -0600

    Modify home page

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -6,20 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  registerMode = false;
-  users: any;
-
   constructor() { }
 
   ngOnInit(): void {
   }
-
-  registerToggle() {
-    this.registerMode = !this.registerMode;
-  }
-
-  cancelRegisterMode(event: boolean) {
-    this.registerMode = event;
-  } 
 
 }
