b216422c25ee82c13f9017a90b492ea03f83c62b
theail03 <andresirias03@gmail.com>
2024-02-24 21:46:35 -0600

    Fix error in prevent unsaved changes file

--- client/src/app/_guards/prevent-unsaved-changes.guard.ts
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
 import { CanDeactivate } from '@angular/router';
 import { Observable, of } from 'rxjs';
-import { MemberEditComponent } from '../members/member-edit/member-edit.component';
 import { ConfirmService } from '../_services/confirm.service';
+import { DatasetComponent } from '../dataset/dataset.component';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PreventUnsavedChangesGuard implements CanDeactivate<MemberEditComponent> {
+export class PreventUnsavedChangesGuard implements CanDeactivate<DatasetComponent> {
   constructor(private confirmService: ConfirmService) {}
 
-  canDeactivate(component: MemberEditComponent): Observable<boolean> {
+  canDeactivate(component: DatasetComponent): Observable<boolean> {
     if (component.editForm?.dirty) {
-      return this.confirmService.confirm()
+      return this.confirmService.confirm();
     }
     return of(true);
   }
