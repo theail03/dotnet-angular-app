1df7290c7ee92e9230c2e974f05c10a5ec64d181
theail03 <andresirias03@gmail.com>
2024-02-27 16:55:35 -0600

    Update environment variables

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'api/'
+  apiUrl: 'api/',
+  googleClientId: "485540724594-jc4vkd4c13m3qndn4kbnbtkgt13rak6m.apps.googleusercontent.com"
 };
